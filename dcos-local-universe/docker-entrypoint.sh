885edf4f3b04e5387b8f5d853e7f9822caf78038
pradeeppanga <pradeepkpanga@gmail.com>
2017-10-05 20:18:54 -0400

    commit

--- dcos-local-universe/docker-entrypoint.sh
+++ dcos-local-universe/docker-entrypoint.sh
@@ -2,22 +2,23 @@
 
 set -e
 
+/usr/sbin/init
+
 set -x \
 	&& systemctl enable docker \
-	&& systemctl start docker 
+	&& systemctl start docker
 
 set -x \
        && git clone https://github.com/mesosphere/universe.git --branch version-3.x \
        && cd universe/docker/local-universe \
        && make base \
-       && make DCOS_VERSION=1.9.2 DCOS_PACKAGE_INCLUDE="$(cat /tmp/package-list)" local-universe
+       && make DCOS_VERSION=1.9.2 DCOS_PACKAGE_INCLUDE="$(cat /tmp/package-list)" local-universe \
+       && docker load < local-universe.tar.gz
 
-set -x \
-       && sh -c "docker load < /universe/docker/local-universe/local-universe.tar.gz"
+#set -x \
+#       && sh -c "docker load < /universe/docker/local-universe/local-universe.tar.gz"
 
 set -x \
        && systemctl daemon-reload \
        && systemctl enable dcos-local-universe-http \
-       && systemctl enable dcos-local-universe-registry \
-       && systemctl start dcos-local-universe-http \
-       && systemctl start dcos-local-universe-registry
+       && systemctl enable dcos-local-universe-registry
